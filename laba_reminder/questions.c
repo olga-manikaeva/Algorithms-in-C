162e621c0e4d71406cb3df281dd0479c5cdef310
OlgaM <olga@MacBook-Air-Olga.local>
2025-03-02 19:10:34 +0100

    Add questions.c

---
+++ laba_reminder/questions.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    
+    // Question 1.4
+    printf("Size of int: %lu bytes\n", sizeof(int));
+    printf("Size of char: %lu bytes\n", sizeof(char));
+    printf("Size of double: %lu bytes\n", sizeof(double));
+
+    return 0;
+}
